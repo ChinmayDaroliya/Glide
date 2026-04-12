1840746dcb675d72b3c5db33cd8069e2235bcb70
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2026-04-12 11:48:15 +0530

    attach post fixed

--- src/components/global/automations/post/index.tsx
+++ src/components/global/automations/post/index.tsx
@@ -10,23 +10,23 @@ import { Button } from '@/components/ui/button'
 import Loader from '../../loader'
 
 type Props = {
-    id:string
+    id: string
 }
 
-const PostButton = ({id}:Props) => {
+const PostButton = ({ id }: Props) => {
 
-    const {data} = useQueryAutomationPosts()
-    const {posts, onSelectPost, mutate, isPending} = useAutomationPosts(id)
+    const { data } = useQueryAutomationPosts()
+    const { posts, onSelectPost, mutate, isPending } = useAutomationPosts(id)
 
     return (
         <TriggerButton label="Attach a post">
             {data?.status === 200 ? (
                 <div className='flex flex-col gap-y-3 w-full'>
                     <div className='flex flex-wrap full gap-3'>
-                        {data?.data?.data?.map((post:InstagramPostProps) => (
+                        {data?.data?.map((post: InstagramPostProps) => (
                             <div className='relative w-4/12 aspect-square rounded-lg cursor-pointer overflow-hidden'
                                 key={post.id}
-                                onClick={() => 
+                                onClick={() =>
                                     onSelectPost({
                                         postid: post.id,
                                         media: post.media_url,
@@ -41,26 +41,26 @@ const PostButton = ({id}:Props) => {
                                         stroke='black'
                                         className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'
                                     />
+                                )}
+                                <Image
+                                    fill
+                                    sizes='100vw'
+                                    src={post.media_url || "/placeholder.png"}
+                                    alt='post image'
+                                    className={cn(
+                                        'hover:opacity-75 transition duration-100',
+                                        posts.find((p: any) => p.postid === post.id) && 'opacity-75'
                                     )}
-                                    <Image
-                                        fill
-                                        sizes='100vw'
-                                        src={post.media_url || "/placeholder.png"}
-                                        alt='post image'
-                                        className={cn(
-                                            'hover:opacity-75 transition duration-100',
-                                            posts.find((p: any) => p.postid === post.id) && 'opacity-75'
-                                        )}
-                                    />
+                                />
                             </div>
                         ))}
                     </div>
-                    <Button onClick={mutate} disabled={posts.length === 0} 
-                    className='bg-gradient-to-br w-full from-[#3352CC] font-medium text-white to-[#1C2D70]'>
+                    <Button onClick={mutate} disabled={posts.length === 0}
+                        className='bg-gradient-to-br w-full from-[#3352CC] font-medium text-white to-[#1C2D70]'>
                         <Loader state={isPending}>Attach Post</Loader>
                     </Button>
                 </div>
-            ):(
+            ) : (
                 <p className='text-text-secondary text-center'>No posts found</p>
             )}
         </TriggerButton>
