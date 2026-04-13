1e07649ddb8c4bc1f871b603fb9907a06758abb2
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2026-04-13 16:10:43 +0530

    removed private message and used only send dm

--- src/app/(protected)/api/webhook/instagram/route.ts
+++ src/app/(protected)/api/webhook/instagram/route.ts
@@ -107,9 +107,9 @@ export async function POST(req: NextRequest) {
                             token: automation.User?.Integrations[0].token?.slice(0, 10)
                         })
 
-                        const direct_message = await sendPrivateMessage(
+                        const direct_message = await sendDM(
                             automation.User?.Integrations[0].instagramId!,
-                            webhook_payload.entry[0].changes[0].value.id,
+                            webhook_payload.entry[0].changes[0].value.from.id,
                             listener?.prompt,
                             automation.User?.Integrations[0].token!
                         )
