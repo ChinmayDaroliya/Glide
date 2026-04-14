dea73dacf82c3dd871f37af3682a0fc5cccd7a90
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2026-04-14 10:51:26 +0530

    trying to get long lived token through generateToken func

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -70,7 +70,7 @@ export const sendPrivateMessage = async (
 }
 
 
-
+// old working code this generates short lived EAA token 
 // export const generateTokens = async (code: string) => {
 //   const redirectUri = getInstagramRedirectUri()
 
@@ -98,29 +98,59 @@ export const sendPrivateMessage = async (
 //   }
 // }
 
+// new working code this generates long lived IAA token
+// export const generateTokens = async (code: string) => {
+//   const redirectUri = getInstagramRedirectUri()
+
+//   const res = await fetch(
+//     `https://graph.facebook.com/v21.0/oauth/access_token` +
+//       `?client_id=${process.env.INSTAGRAM_CLIENT_ID}` +
+//       `&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}` +
+//       `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+//       `&code=${code}` +
+//       `&grant_type=authorization_code`
+//   )
+
+//   const data = await res.json()
+//   if (!data.access_token) return null
+
+//   const ig = await fetch(
+//     `https://graph.facebook.com/v21.0/me?fields=user_id,username&access_token=${data.access_token}`
+//   )
+
+//   const igData = await ig.json()
+
+//   return {
+//     access_token: data.access_token,
+//     instagramId: igData.user_id
+//   }
+// }
+
+
+// testing...
+// test this in morning
 export const generateTokens = async (code: string) => {
   const redirectUri = getInstagramRedirectUri()
 
+  const form = new URLSearchParams()
+  form.append("client_id", process.env.INSTAGRAM_CLIENT_ID!)
+  form.append("client_secret", process.env.INSTAGRAM_CLIENT_SECRET!)
+  form.append("grant_type", "authorization_code")
+  form.append("redirect_uri", redirectUri)
+  form.append("code", code)
+
   const res = await fetch(
-    `https://graph.facebook.com/v21.0/oauth/access_token` +
-      `?client_id=${process.env.INSTAGRAM_CLIENT_ID}` +
-      `&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}` +
-      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-      `&code=${code}` +
-      `&grant_type=authorization_code`
+    "https://api.instagram.com/oauth/access_token",
+    {
+      method: "POST",
+      body: form
+    }
   )
 
   const data = await res.json()
-  if (!data.access_token) return null
-
-  const ig = await fetch(
-    `https://graph.facebook.com/v21.0/me?fields=user_id,username&access_token=${data.access_token}`
-  )
-
-  const igData = await ig.json()
 
   return {
-    access_token: data.access_token,
-    instagramId: igData.user_id
+    access_token: data.access_token, // IGAA token
+    instagramId: data.user_id
   }
 }
